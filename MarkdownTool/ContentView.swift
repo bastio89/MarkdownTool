80caf4cfc267d78ab2d8900f96c44c3dabaee97c
Sebastian Oczachowski <sebastian.oczachowski@finnova.com>
2026-06-02 21:03:02 +0200

    Initial Commit

---
+++ MarkdownTool/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MarkdownTool
+//
+//  Created by Oczachowski Sebastian on 02.06.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MarkdownTool/MarkdownToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarkdownToolApp.swift
+//  MarkdownTool
+//
+//  Created by Oczachowski Sebastian on 02.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct MarkdownToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
